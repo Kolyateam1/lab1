54bdb25bac8e25a8752cd14c077a2020cbf13958
kolya_team <kolya.bahurov@gmail.com>
2025-10-15 12:12:57 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
 import java.util.Random;
 
 public class Main {
+    public static double f1 (double k1) {
+        return Math.sin(Math.atan(Math.pow((k1-0.5)/23,2)));
+    }
+    public static double f2 (double k2) {
+        return Math.exp(Math.tan(Math.sin(k2)));
+    }
+    public static double f3 (double k3) {
+        return Math.sin(Math.sin(Math.pow((4 *(0.25 + Math.atan((k3-0.5)/23))), 3)));
+    }
     public static void main(String[] args){
         int [] w = new int[9];
         for (int i = 0; i < 9; i++){
@@ -16,13 +25,13 @@ public class Main {
             for (int j = 0; j < 14; j++){
                 double X = x[j];
                 if (w[i] == 4){
-                    s[i][j] = Math.sin(Math.atan(Math.pow((X-0.5)/23,2)));
+                    s[i][j] = f1 (X);
                 }
                 else if (w[i] == 6 || w[i] == 8 || w[i] == 14 || w[i] == 18){
-                    s[i][j] = Math.exp(Math.tan(Math.sin(X)));
+                    s[i][j] = f2(X);
                 }
                 else {
-                    s[i][j] = Math.sin(Math.sin(Math.pow((4 *(0.25 + Math.atan((X-0.5)/23))), 3)));
+                    s[i][j] = f3(X);
                 }
             }
         }
