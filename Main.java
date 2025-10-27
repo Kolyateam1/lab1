fb4124c3e3c4770195f94c328e53f6a35a6e3c95
kolya_team <kolya.team.7@gmail.com>
2025-10-27 20:15:58 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,3 @@
-import java.util.Random;
-
 public class Main {
     public static double f1 (double k1) {
         return Math.sin(Math.atan(Math.pow((k1-0.5)/23,2)));
@@ -10,22 +8,29 @@ public class Main {
     public static double f3 (double k3) {
         return Math.sin(Math.sin(Math.pow((4 *(0.25 + Math.atan((k3-0.5)/23))), 3)));
     }
+    public static void f4(double[][] z){
+        for (int i = 0; i < 9; i++){
+            for (int j = 0; j < 14; j++){
+                    System.out.printf("%-10.5f | ", z[i][j]);
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args){
         int [] w = new int[9];
         for (int i = 0; i < 9; i++){
             w[i] = 4 + i*2;
         }
         double [] x = new double[14];
-        Random random = new Random();
         for (int j = 0; j < 14; j++){
-            x[j] = -12.0 + random.nextDouble() * (11.0 - (-12.0));
+            x[j] = -12.0 + Math.random() * (11.0 - (-12.0));
         }
         double [][] s = new double[9][14];
         for (int i = 0; i < 9; i++){
             for (int j = 0; j < 14; j++){
                 double X = x[j];
                 if (w[i] == 4){
-                    s[i][j] = f1 (X);
+                    s[i][j] = f1(X);
                 }
                 else if (w[i] == 6 || w[i] == 8 || w[i] == 14 || w[i] == 18){
                     s[i][j] = f2(X);
@@ -35,11 +40,6 @@ public class Main {
                 }
             }
         }
-        for (int i = 0; i < 9; i++){
-            for (int j = 0; j < 14; j++){
-                System.out.printf("%10.5f", s[i][j]);
-            }
-            System.out.println();
-        }
+    f4(s);
     }
 }
